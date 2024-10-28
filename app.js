24d345adc94ce60378e0505241800f2e85ecd718
dennisbkd <rasguidorondalesdennis@gmail.com>
2024-10-27 22:39:59 -0400

    Deploy API to  Vercel

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+const {validateMovie, validatePartialMovie} = require('./Schema/movies.js')
+
+const app = express()
+
+app.use(express.json())
+
+app.disable('x-powered-by')
+
+app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+
+  const { genre } = req.query
+  if (genre) {
+    const filterdMovies = movies.filter(movie => {
+      return movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase())
+    })
+    return res.json(filterdMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const {id} = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({message: 'movie not found'})
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({error: JSON.parse(result.error.message)})
+  }
+  const newMovies = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovies)
+
+  res.status(201).json(newMovies)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({error: JSON.parse(result.error.message)})
+  }
+
+  const {id} = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({message: 'movie not found'})
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`server listening on por http://localhost:${PORT}`)
+})
