aeaa676296892adaa5bd8399b74327cc7a3e7c78
dennisbkd <rasguidorondalesdennis@gmail.com>
2024-10-30 18:52:26 -0400

    modificando la url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(json())
 
 app.disable('x-powered-by')
 
-app.get('./Schema/movies', (req, res) => {
+app.get('/Schema/movies', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
 
   const { genre } = req.query
@@ -25,7 +25,7 @@ app.get('./Schema/movies', (req, res) => {
   res.json(movies)
 })
 
-app.get('./Schema/movies/:id', (req, res) => {
+app.get('/Schema/movies/:id', (req, res) => {
   const {id} = req.params
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
@@ -33,7 +33,7 @@ app.get('./Schema/movies/:id', (req, res) => {
   res.status(404).json({message: 'movie not found'})
 })
 
-app.post('./Schema/movies', (req, res) => {
+app.post('/Schema/movies', (req, res) => {
   const result = validateMovie(req.body)
 
   if (result.error) {
@@ -49,7 +49,7 @@ app.post('./Schema/movies', (req, res) => {
   res.status(201).json(newMovies)
 })
 
-app.patch('./Schema/movies/:id', (req, res) => {
+app.patch('/Schema/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
   if (!result.success) {
     return res.status(400).json({error: JSON.parse(result.error.message)})
